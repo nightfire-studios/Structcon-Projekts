7aa79d8344de6effc8f72dc8a3f3694dd7f456e4
Dennis Danso - Nightfire Studios <kumidendanso@gmail.com>
2024-10-09 13:41:16 +0000

    About page setup complete

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 import {gsap} from "gsap";
 import {navHover} from '/nav_menus.js'
 import {carousel} from '/carousel.js'
-import {projectFunctions} from './projects.js'
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAmwGlYzOXmGBBvhQW_W1m7sAAOqIKVOGU",
-  authDomain: "structcon-projekts-f4b99.firebaseapp.com",
-  projectId: "structcon-projekts-f4b99",
-  storageBucket: "structcon-projekts-f4b99.appspot.com",
-  messagingSenderId: "985867633038",
-  appId: "1:985867633038:web:09e5da0084443eb343e4df",
-  measurementId: "G-JPWGX4JLZF"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+import {galleryCarousel, projectCarausel, projectFunctions,intersetion_observer} from '/projects.js'
 
 
 
@@ -33,13 +11,23 @@ navHover()
 carousel()
 
 } catch (error) {
-  
+  console.error('Error in navHover or carousel:', error);
 }
 
 try {
-  projectFunctions() 
-} catch (error) {
   
+  projectFunctions()
+
+} catch (error) {
+  console.error('Error in pro:', error);
+}
+
+try {
+  projectCarausel() 
+  galleryCarousel()
+  intersetion_observer()
+} catch (error) {
+  console.error('Error in pc:', error);
 }
 
 
--- projects.js
+++ projects.js
@@ -6,7 +6,32 @@ const project_size = document.querySelector('.project_size')
 const project_time = document.querySelector('.project_time')
 const project_location = document.querySelector('.project_location')
 
-export const projectFunctions = ()=>{
+const project_arrow_left = document.querySelector('.project_arrow_left')
+const project_arrow_right = document.querySelector('.project_arrow_right')
+const projects_trey = document.querySelector('.project_trey')
+const totalItems = document.querySelectorAll('.project_trey img').length
+var itemIndex = 1
+const moveValue = 82
+var value = 0
+const projects_count = document.querySelector('.projects_count')
+const projects_total = document.querySelector('.projects_total')
+const gallery_count = document.querySelector('.gallery_count')
+const gallery_total = document.querySelector('.gallery_length')
+
+const gallery_arrow_left = document.querySelector('.gallery_arrow_left')
+const gallery_arrow_right = document.querySelector('.gallery_arrow_right')
+const gallery_trey = document.querySelector('.gallery_trey')
+
+const gallery_length = document.querySelectorAll('.gallery_trey img').length
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    projects_total.textContent = '0' + totalItems
+    gallery_total.textContent = '0' + gallery_length
+})
+
+
+export const projectFunctions = () => {
     const getScrollIndex = () => {
 
 
@@ -21,7 +46,7 @@ export const projectFunctions = ()=>{
 
             const distance = Math.abs(projectCenter - treyCenter)
 
-          
+
 
             if (distance < infinitNumber) {
                 infinitNumber = distance
@@ -40,27 +65,113 @@ export const projectFunctions = ()=>{
 
         projects.forEach((items, index) => {
             if (index === project_index) {
-                project_count.innerHTML = '0' + (index+1)
+                project_count.innerHTML = '0' + (index + 1)
 
-                if(index === 0){
+                if (index === 0) {
                     property_name.textContent = '- Grace Heights Homes'
                     project_size.textContent = '388sqm'
                     project_time.textContent = '2023 - TBD'
                     project_location.textContent = 'Adjrigano - West Legon'
-                }else if(index === 1){
-                    property_name.textContent = '- Josh homes'
+                } else if (index === 1) {
+                    property_name.textContent = '- Abetifi Container Project'
                     project_size.textContent = '415sqm'
                     project_time.textContent = '2023 - TBD'
-                    project_location.textContent = 'American House - East Legon'
-                }else if(index === 2){
-                    property_name.textContent = '- Rosey Homes'
+                    project_location.textContent = 'Abetifi'
+                } else if (index === 2) {
+                    property_name.textContent = '- Josh homes'
                     project_size.textContent = '355sqm'
                     project_time.textContent = '2024 - TBD'
-                    project_location.textContent = 'Dorwenya'
+                    project_location.textContent = 'East Legon - American House'
                 }
 
-                
+
             }
         })
     })
+}
+
+export const projectCarausel = () => {
+
+    console.log(totalItems)
+
+    project_arrow_right.onclick = () => {
+        if (itemIndex < totalItems) {
+            value += moveValue
+            projects_trey.style.transform = `translateX(-${value}%)`
+            itemIndex++
+            projects_count.textContent = '0' + itemIndex
+        }
+    }
+
+    project_arrow_left.onclick = () => {
+        if (itemIndex > 1) {
+            itemIndex--
+            value -= moveValue
+            projects_trey.style.transform = `translateX(-${value}%)`
+            projects_count.textContent = '0' + itemIndex
+        }
+    }
+
+}
+
+export const galleryCarousel = () => {
+
+    var gallery_index = 1
+    var gallery_value = 0
+    const gallery_moveValue = 100
+
+    gallery_arrow_right.onclick = () => {
+        if (gallery_index < gallery_length) {
+            gallery_value += gallery_moveValue
+            gallery_trey.style.transform = `translateX(-${gallery_value}%)`
+            gallery_index++
+            gallery_count.textContent = '0' + gallery_index
+        }
+    }
+
+    gallery_arrow_left.onclick = () => {
+        if (gallery_index > 1) {
+            gallery_index--
+            gallery_value -= gallery_moveValue
+            gallery_trey.style.transform = `translateX(-${gallery_value}%)`
+            gallery_count.textContent = '0' + gallery_index
+
+        }
+    }
+
+
+
+}
+
+export const intersetion_observer = ()=>{
+
+    var projectImages = document.querySelectorAll('.content_img img')
+    var content1 = document.querySelector('.content1')
+    var content3 = document.querySelector('.content3')
+    var project_breaker = document.querySelector('.project_breaker')
+    var observeArray = [content1,project_breaker,content3]
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: .2
+
+    }
+
+   const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+            
+        }
+    })
+   },options)
+
+   projectImages.forEach(items=>{
+    observer.observe(items)
+   })
+
+   observeArray.forEach(items=>{
+    observer.observe(items)
+   })
 }
